fc36efdad3eb2b5d74a2d187f27a6d3c38261c3e
deFAT64 <>
2025-08-17 10:29:30 +0200

    Add gitignore, mod build.sh and move robots.txt

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 mkdocs build --config-file ./meshcore-manual-pl/mkdocs.yml -d /var/www/html
+rm -R /gitea/meshcoremanualpl/www/*
+cp -R /var/www/html/* /gitea/meshcoremanualpl/www
+cp /gitea/meshcoremanualpl/meshcore-manual-pl/scripts/robots.txt /gitea/meshcoremanualpl/www
+cp /gitea/meshcoremanualpl/meshcore-manual-pl/scripts/.htaccess /gitea/meshcoremanualpl/www
